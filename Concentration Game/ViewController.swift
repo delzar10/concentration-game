e8fa56f91f9dcc6a67da23cd68fae5651f03a4ce
Miguel Angel Delgadillo Zarate <mikydelzar@2806-102e-001c-7e33-8cf3-e150-becf-38df.ipv6.infinitum.net.mx>
2019-02-26 19:02:23 -0600

    Initial Commit

---
+++ Concentration Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration Game
+//
+//  Created by Miguel Angel Delgadillo Zarate on 2/26/19.
+//  Copyright © 2019 Miguel Angel Delgadillo Zarate. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
